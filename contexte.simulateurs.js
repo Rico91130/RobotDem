401853f010f2ea94223e7c9ad745141f5e40050d
Rico91130 <76157511+Rico91130@users.noreply.github.com>
2023-08-31 11:59:36 +0200

    Update contexte.simulateurs.js

--- contexte.simulateurs.js
+++ contexte.simulateurs.js
@@ -35,12 +35,6 @@ function _ContextualizedGetField(i, etape, domObj) {
         type = "select";
     }
 
-    /* Cas des search (TODO : A améliorer) */
-    if (domObj.type == "search" || (domObj.type == "text" && domObj.classList.contains("autocomplete"))) {
-        type = "autocomplete";
-        argument = '{"searchString":"' + domObj.value.replace("'", "\\\'") + '", "dropdownItemIndex" : 0}';
-    }
-
     return [
         i,
         etape,
@@ -140,98 +134,6 @@ function _ContextualizedExecute() {
                 }
                 this.done = true;
                 break;
-            case "autocomplete":
-
-                /* Traitement initial comme un textbox */
-                this.getItem().click();
-                if (!this.getItem().disabled) {
-
-                    /* Dans le cas où c'est un objet JSON en argument */
-                    if (this.args.searchString != null)
-                        this.getItem().value = this.args.searchString;
-                    /* Autrement si c'est une chaine de caractère */
-                    else if (this.args.value != null)
-                        this.getItem().value = this.args.value;
-                }
-
-                /* Déclenchement de l'autocomplete */
-                this.getItem().dispatchEvent(new Event('input', { bubbles: true }));
-
-                /* On attend le chargement de la liste de résultat */
-                let _this = this;
-                var autocompleteContainer = null;
-                var catchAutocompleteContainerULFunction = (e) => {
-                    /* Cas UL/LI */
-                    if (e.relatedNode.nodeName == "UL" && e.srcElement.nodeName == "LI") {
-                        autocompleteContainer = e.relatedNode;
-                        /* Cas DIV/DIV */
-                    } else if (e.relatedNode.nodeName == "DIV" && e.srcElement.nodeName == "DIV" && e.srcElement.classList.contains("a11y-suggestion")) {
-                        autocompleteContainer = e.relatedNode;
-                    }
-                }
-                document.body.addEventListener("DOMNodeInserted", catchAutocompleteContainerULFunction);
-
-                let interval = setInterval(function () {
-                    if (autocompleteContainer != null && autocompleteContainer.style["display"] != "none" && autocompleteContainer.children.length >= _this.args.dropdownItemIndex) {
-                        autocompleteContainer.children[_this.args.dropdownItemIndex].click();
-                        document.body.removeEventListener("DOMNodeInserted", catchAutocompleteContainerULFunction);
-                        _this.done = true;
-                        clearInterval(interval);
-                    }
-                }, 200);
-
-                break;
-
-            case "asyncUploadTMA":
-
-                if (this.getItem().closest(".pslUploadZoneSaisie").style["display"] != "none") {
-
-                    let _this = this;
-
-                    $(window).focus();
-                    $(window).on("focus", function () {
-                        _this.windowFocus = true;
-                    });
-                    this.getItem().click();
-
-                    let interval = setInterval(function () {
-                        if (_this.windowFocus || _this.getItem().closest(".pslUploadZoneSaisie").style["display"] == "none") {
-                            _this.done = true;
-                            clearInterval(interval);
-                        }
-                    }, 100);
-
-                } else {
-                    this.done = true;
-                }
-                break;
-
-
-            case "asyncUploadV2":
-
-                if (this.getItem().attr("class") == undefined || this.getItem().attr("class").indexOf("thHide") == -1) {
-
-                    let _this = this;
-
-                    $(window).focus();
-                    $(window).on("focus", function () {
-                        console.log("focus");
-                        _this.windowFocus = true;
-                    });
-                    this.getItem().find('button.btn-upload').click();
-
-                    let interval = setInterval(function () {
-                        if (_this.windowFocus || (_this.getItem().attr("class") != undefined && _this.getItem().attr("class").indexOf("thHide") != -1)) {
-                            _this.done = true;
-                            clearInterval(interval);
-                        }
-                    }, 100);
-
-                } else {
-                    this.done = true;
-                }
-                break;
-
         }
     }
 }
